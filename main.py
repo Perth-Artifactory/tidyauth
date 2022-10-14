51d6bb7f52845d84ccc1ddf33f35e1bc4e3c6a0b
Fletcher <me@fboyd.me>
2022-10-14 12:16:52 +0800

    Variable routes and logging

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
 import json
-import requests
+import logging
+
 import flask
-from flask import request, jsonify
+import requests
+from flask import jsonify, request
 
 with open("config.json","r") as f:
     config = json.load(f)
-    
+
+if config["server"]["debug"]:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 tokens = config["tokens"]
 
 app = flask.Flask(__name__)
@@ -15,38 +22,37 @@ app = flask.Flask(__name__)
 def backup(zone, mode, k=None):
     if mode == "r":
         with open("backup.{}.json".format(zone), mode) as f:
-            print("Loading keys from file cache...")
+            logging.debug("Loading keys from file cache...")
             try:
                 k = json.load(f)
             except FileNotFoundError:
                 return {}
             except json.decoder.JSONDecodeError:
+                logging.warning(f"backup.{zone}.json has invalid JSON. Has it been edited by something else?")
                 return {}
-            print("Loaded keys, {} keys found".format(len(k)))
+            logging.debug(f"Loaded keys, {len(k)} keys found")
             return k
     
     elif mode == "w":
         with open("backup.{}.json".format(zone), mode) as f:
             json.dump(k,f, indent=4)
-            print("{} keys written to backup file: backup.{}.json".format(len(k),zone))
+            logging.debug(f"{len(k)} keys written to backup file: backup.{zone}.json")
             return True
     return False
 
 def pull():
     # Get all contact information from TidyHQ
-    print("Attempting to refresh keys from TidyHQ...")
+    logging.debug("Attempting to refresh keys from TidyHQ...")
     try:
-        print("Polling TidyHQ")
         r = requests.get(config["urls"]["contacts"],params={"access_token":config["tidytoken"]})
         contacts = r.json()
     except requests.exceptions.RequestException as e:
-        print("Could not reach TidyHQ")
+        logging.error("Could not reach TidyHQ")
         return False
-    print("Received response from TidyHQ")
     return contacts
 
 def process(zone, contacts=None):
-    if not contacts:
+    if not contacts and zone[-4:] == "keys":
         contacts = pull()
 
     keys = {}
@@ -94,15 +100,13 @@ def process(zone, contacts=None):
     
     elif zone == "vending.data":
         # Get drink information
-        print("Attempting to get option data from TidyHQ...")
+        logging.debug("Attempting to get option data from TidyHQ...")
         try:
-            print("Polling TidyHQ")
-            r = requests.get(config["urls"]["fields"]+"/"+config["ids"]["drink"],params={"access_token":config["tidytoken"]})
+            r = requests.get(f"{config['urls']['fields']}/{config['ids']['drink']}",params={"access_token":config["tidytoken"]})
             d = r.json()
         except requests.exceptions.RequestException as e:
-            print("Could not reach TidyHQ")
+            logging.error("Could not reach TidyHQ")
             return False
-        print("Received response from TidyHQ")
         drinks = {}
         for drink in d["choices"]:
             drinks[drink["id"]] = {"name": drink["title"].strip(),
@@ -112,79 +116,37 @@ def process(zone, contacts=None):
         return drinks
 
     # Generic notification and saving
-    print("Updated keys for zone:{}, {} keys processed".format(zone,len(keys)))
+    logging.debug(f"Updated keys for zone:{zone}, {len(keys)} keys processed")
     backup(zone=zone, mode="w", k=keys)
     return keys
 
-@app.route('/api/v1/keys/door', methods=['GET'])
-def query_door():
+@app.route('/api/v1/<type>/<item>', methods=['GET'])
+def config_vending(type,item):
     global data
-    zone="door.keys"
+    zone = f"{item}.{type}"
+    if zone not in zones:
+        return jsonify({'message':f"{zone} is not a valid zone"}), 401
     token = request.args.get('token')
     up = request.args.get('update')
     if token not in tokens:
         return jsonify({'message':'Send a valid token'}), 401
 
     if up == "tidyhq":
+        logging.info(f"{request.environ['REMOTE_ADDR']} using token:<{token}> requested a pull from TidyHQ for {zone}")
         k = process(zone=zone)
         if not k:
             return jsonify({'message':'Could not reach TidyHQ'}), 502
         data[zone] = k
     elif up == "file":
+        logging.info(f"{request.environ['REMOTE_ADDR']} using token:<{token}> requested a pull from file for {zone}")
         data[zone] = backup(zone=zone, mode="r")
+    else:
+        logging.info(f"{request.environ['REMOTE_ADDR']} using token:<{token}> requested cached data for {zone}")
 
     if data[zone]:
         return jsonify(data[zone])
     else:
-        print("No keys in cache, pulling from TidyHQ")
-        data[zone] = process(zone=zone)
-        return jsonify(data[zone])
-
-@app.route('/api/v1/keys/vending', methods=['GET'])
-def query_vending():
-    global data
-    zone="vending.keys"
-    token = request.args.get('token')
-    up = request.args.get('update')
-    if token not in tokens:
-        return jsonify({'message':'Send a valid token'}), 401
-
-    if up == "tidyhq":
-        k = process(zone=zone)
-        if not k:
-            return jsonify({'message':'Could not reach TidyHQ'}), 502
-        data[zone] = k
-    elif up == "file":
-        data[zone] = backup(zone=zone, mode="r")
-
-    if data[zone]:
-        return jsonify(data[zone])
-    else:
-        print("No keys in cache, pulling from TidyHQ")
-        data[zone] = process(zone=zone)
-        return jsonify(data[zone])
-
-@app.route('/api/v1/data/vending', methods=['GET'])
-def config_vending():
-    global data
-    zone="vending.data"
-    token = request.args.get('token')
-    up = request.args.get('update')
-    if token not in tokens:
-        return jsonify({'message':'Send a valid token'}), 401
-
-    if up == "tidyhq":
-        k = process(zone=zone)
-        if not k:
-            return jsonify({'message':'Could not reach TidyHQ'}), 502
-        data[zone] = k
-    elif up == "file":
-        data[zone] = backup(zone=zone, mode="r")
-
-    if data[zone]:
-        return jsonify(data[zone])
-    else:
-        print("No keys in cache, pulling from TidyHQ")
+        logging.debug("No keys in cache, pulling from TidyHQ")
         data[zone] = process(zone=zone)
         return jsonify(data[zone])
 
@@ -193,7 +155,9 @@ def home():
     token = request.args.get('token')
     if config["server"]["debug"]:
         if token not in tokens:
+            logging.info(f"Invalid token <{token}> from {request.environ['REMOTE_ADDR']}")
             return jsonify({'message':"You've successfuly reached the auth service but need to provide a token to continue"}), 401
+        logging.debug(f"Valid token <{token}> from {request.environ['REMOTE_ADDR']} received to test endpoint")
         return jsonify({'message':"You're good to go!"}), 200
     return jsonify({'message':"You're good to go!"}), 200
 
@@ -203,19 +167,19 @@ if __name__ == "__main__":
     c = pull()
     for zone in zones:
         if zone[-4:] == "keys":
+            logging.debug(f"Initial pull for {zone}")
             data[zone] = process(zone=zone, contacts=c)
         else:
             data[zone] = process(zone=zone)
         if not data[zone]:
-            print("Could not pull from TidyHQ, loading file backup")
+            logging.error("Could not pull from TidyHQ, loading file backup")
             data[zone] = backup(zone=zone, mode="r")
         
     from waitress import serve
-    print("Auth server starting on {address}:{port}...".format(**config["server"]))
     if config["server"]["debug"]:
-        print("WARNING: Debug mode enabled. Do not use in production!")
+        logging.warning("Debug mode enabled. Do not use in production!")
     if "DEMO CLIENT" in tokens:
-        print("WARNING: Demo client token active. Do not use in production!")
+        logging.warning("Demo client token active. Do not use in production!")
     if "List of authentication tokens" in tokens:
-        print("WARNING: Placeholder token active. Do not use in production!")
+        logging.error("Placeholder token active. Do not use in production!")
     serve(app, host=config["server"]["address"], port=config["server"]["port"])
