5e02b75394793a229a81035ad344daa1e0d03a6e
Fletcher <me@fboyd.me>
2022-10-16 16:58:43 +0800

    Locker assignments in JSON format

--- scripts/lockers.py
+++ scripts/lockers.py
@@ -2,9 +2,17 @@
 
 import itertools
 import json
+import sys
 from pprint import pprint
 
 import util
+
+if len(sys.argv) < 2:
+    output_format = "json"
+elif sys.argv[1] not in ["json","html"]:
+    output_format = "json"
+else:
+    output_format = sys.argv[1]
 
 try:
     with open("./config.json") as f:
@@ -17,7 +25,7 @@ contacts = util.pull(config=config)
 
 lockers = {}
 for contact in contacts:
-    locker = util.find(contact = contact, field_id=config["ids"]["locker"])
+    locker = util.find(contact=contact, field_id=config["ids"]["locker"])
     if locker:
         lockers[contact["id"]] = locker
 
@@ -27,5 +35,23 @@ for locker in lockers:
     if s[0] not in locations:
         locations[s[0]] = {}
     locations[s[0]][s[1]] = locker
-pprint(locations)
-print(f"found {len(lockers)} locker assignments")
+
+if output_format == "json":
+    pprint(locations)
+    sys.exit(0)
+elif output_format == "html":
+    print("I'm not ready yet!")
+    sys.exit(0)
+for location in sorted(locations):
+    print(f"Area: {location}")
+    l = 1
+    for locker in sorted(locations[location]):
+        lp = int(locker)
+        while lp > l+1:
+            l += 1
+            print(f"{l:0{len(str(locker))}}: NO DATA")
+        l = int(locker)
+        contact_id = locations[location][locker]
+        name = util.prettyname(contact_id=contact_id, config=config, contacts=contacts)
+        member_status = util.check_membership(contact_id=locations[location][locker], config=config)
+        print(f"{locker}: {name} ({contact_id}) - Member: {member_status}")
