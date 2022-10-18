66bdef3bab801a969fc1315eab9a2a15f08bf3d9
Fletcher <me@fboyd.me>
2022-10-18 17:24:39 +0800

    Add member report to all_reports

--- scripts/all_reports.py
+++ scripts/all_reports.py
@@ -5,9 +5,10 @@ from pprint import pprint
 import audit_emergency
 import locker_allocation
 import locker_utilisation
+import members
 import template
 
-reporters = [template, locker_allocation, locker_utilisation, audit_emergency]
+reporters = [template, members, locker_allocation, locker_utilisation, audit_emergency]
 reports = []
 for reporter in reporters:
     reports.append(reporter.chain())
