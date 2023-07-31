10e5486f91746d95557ba31c7510e4351ff8d457
Fletcher <me@fboyd.me>
2023-07-31 16:21:26 +0800

    Further formatting

--- scripts/locker_utilisation.py
+++ scripts/locker_utilisation.py
@@ -13,11 +13,12 @@ def chain():
     global s
     return s
 
+
 if __name__ != "__main__":
     output_format = "internal"
 elif len(sys.argv) < 2:
     output_format = "string"
-elif sys.argv[1] not in ["json","html","html_embed","mrkdwn","string"]:
+elif sys.argv[1] not in ["json", "html", "html_embed", "mrkdwn", "string"]:
     output_format = "string"
 else:
     output_format = sys.argv[1]
@@ -45,17 +46,17 @@ for person in lockers:
     s = ["".join(x) for _, x in itertools.groupby(lockers[person], key=str.isdigit)]
     if s[0] not in locations:
         locations[s[0]] = {}
-    locations[s[0]][s[1]] = {"membership": util.check_membership(contact_id=person, config=config)}
+    locations[s[0]][s[1]] = {
+        "membership": util.check_membership(contact_id=person, config=config)
+    }
 
-counts = {"Active members":0,
-          "Expired members": 0,
-          "Blank": 0}
+counts = {"Active members": 0, "Expired members": 0, "Blank": 0}
 for location in sorted(locations):
     l = 1
     for locker in sorted(locations[location]):
         data = locations[location][locker]
         lp = int(locker)
-        while lp > l+1:
+        while lp > l + 1:
             l += 1
             counts["Blank"] += 1
         l = int(locker)
@@ -65,24 +66,29 @@ for location in sorted(locations):
             counts["Expired members"] += 1
 
 t = 0
-for c in counts: t += counts[c]
+for c in counts:
+    t += counts[c]
 counts["Total"] = t
 
 if output_format == "json":
     pprint(counts)
     sys.exit(0)
-elif output_format in ["html","html_embed", "mrkdwn","internal"]:
+elif output_format in ["html", "html_embed", "mrkdwn", "internal"]:
     d = [["Type", "#", "%"]]
     for c in counts:
         d.append([c, counts[c], f'{round(counts[c] / counts["Total"]*100)}%'])  # type: ignore
         if c == "Total":
             d[-1].pop()
 
-    s = [{"title":"Locker utilisation",
-         "explainer": f"This table has been generated from data stored in TidyHQ. It was retrieved at: {datetime.now()}",
-         "table": d}]
+    s = [
+        {
+            "title": "Locker utilisation",
+            "explainer": f"This table has been generated from data stored in TidyHQ. It was retrieved at: {datetime.now()}",
+            "table": d,
+        }
+    ]
     if output_format == "html_embed":
-        print(util.report_formatter(data = [{"table":d}], dtype = output_format))
+        print(util.report_formatter(data=[{"table": d}], dtype=output_format))
     elif output_format != "internal":
         print(util.report_formatter(data=s, dtype=output_format))
 
--- scripts/members.py
+++ scripts/members.py
@@ -15,11 +15,12 @@ def chain():
     global s
     return s
 
+
 if __name__ != "__main__":
     output_format = "internal"
 elif len(sys.argv) < 2:
     output_format = "string"
-elif sys.argv[1] not in ["json","html","html_embed","mrkdwn","string"]:
+elif sys.argv[1] not in ["json", "html", "html_embed", "mrkdwn", "string"]:
     output_format = "string"
 else:
     output_format = sys.argv[1]
@@ -36,14 +37,19 @@ if not contacts:
     sys.exit(1)
 
 try:
-    r = requests.get(config["urls"]["memberships"],params={"access_token":config["tidytoken"]})
+    r = requests.get(
+        config["urls"]["memberships"], params={"access_token": config["tidytoken"]}
+    )
     memberships = r.json()
 except requests.exceptions.RequestException as e:
     logging.error("Could not reach TidyHQ")
     sys.exit(1)
 
 try:
-    r = requests.get(config["urls"]["membership_levels"],params={"access_token":config["tidytoken"]})
+    r = requests.get(
+        config["urls"]["membership_levels"],
+        params={"access_token": config["tidytoken"]},
+    )
     levels = r.json()
 except requests.exceptions.RequestException as e:
     logging.error("Could not reach TidyHQ")
@@ -52,45 +58,62 @@ except requests.exceptions.RequestException as e:
 membership_levels = {}
 
 for level in levels:
-    if not level['deleted']:
-        if level["amount"] != '0.0':
+    if not level["deleted"]:
+        if level["amount"] != "0.0":
             if level["unit_period"] == "year":
                 level["duration"] = level["duration"] * 12
             cost = float(level["amount"]) / level["duration"]
         else:
             cost = 0
-        membership_levels[level['id']] = {"name": level['name'],
-                                          "count": 0,
-                                          "cost": cost}
+        membership_levels[level["id"]] = {
+            "name": level["name"],
+            "count": 0,
+            "cost": cost,
+        }
 
 total_memberships = 0
 total_money = 0
 for membership in memberships:
-    if membership['state'] != "expired" and membership['membership_level_id'] in membership_levels.keys():
-        membership_levels[membership['membership_level_id']]["count"] += 1
+    if (
+        membership["state"] != "expired"
+        and membership["membership_level_id"] in membership_levels.keys()
+    ):
+        membership_levels[membership["membership_level_id"]]["count"] += 1
         total_memberships += 1
-        total_money += membership_levels[membership['membership_level_id']]["cost"]
+        total_money += membership_levels[membership["membership_level_id"]]["cost"]
 
-membership_levels = sorted(membership_levels.items(), key=lambda i: i[1]['count'], reverse=True)
+membership_levels = sorted(
+    membership_levels.items(), key=lambda i: i[1]["count"], reverse=True
+)
 
 if output_format == "json":
     pprint(membership_levels)
     sys.exit(0)
-elif output_format in ["html", "html_embed", "mrkdwn","internal"]:
-    d = [["Membership", "#", "%","$","$%"]]
+elif output_format in ["html", "html_embed", "mrkdwn", "internal"]:
+    d = [["Membership", "#", "%", "$", "$%"]]
     for m in membership_levels:
         m = m[1]
         d.append([m["name"], m["count"], f'{round(m["count"] / total_memberships*100)}%', f'${round(m["count"]*m["cost"])}/mo', f'{round(m["count"]*m["cost"] / total_money*100)}%'])  # type: ignore
-    d.append(["Total", total_memberships, " ", f'${round(total_money)}/mo', " "])  # type: ignore
-    s = [{"title":"Membership stats",
-         "explainer": f"This table has been generated from data stored in TidyHQ. It was retrieved at: {datetime.now()}",
-         "table": d}]
+    d.append(["Total", total_memberships, " ", f"${round(total_money)}/mo", " "])  # type: ignore
+    s = [
+        {
+            "title": "Membership stats",
+            "explainer": f"This table has been generated from data stored in TidyHQ. It was retrieved at: {datetime.now()}",
+            "table": d,
+        }
+    ]
     if output_format == "html_embed":
-        print(util.report_formatter(data = [{"table":d}], dtype = output_format))
+        print(util.report_formatter(data=[{"table": d}], dtype=output_format))
     elif output_format != "internal":
         print(util.report_formatter(data=s, dtype=output_format))
 
 elif output_format == "string":
     for m in membership_levels:
         m = m[1]
-        print(m["name"], m["count"], f'{round(m["count"] / total_memberships*100)}%', f'${round(m["count"]*m["cost"])}/mo', f'{round(m["count"]*m["cost"] / total_money*100)}%')
+        print(
+            m["name"],
+            m["count"],
+            f'{round(m["count"] / total_memberships*100)}%',
+            f'${round(m["count"]*m["cost"])}/mo',
+            f'{round(m["count"]*m["cost"] / total_money*100)}%',
+        )
